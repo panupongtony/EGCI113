b88cfbc2f5191ab4ecbd4e2b1ff2ede4d0b15b7c
panupongtony <146054375+panupongtony@users.noreply.github.com>
2023-09-27 11:44:35 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+
 int main()
 {
-    int a,b,c;
-    float x1,x2;
-    printf("Enter a,b,c,:");
-    scanf("%d,%d,%d",&a,&b,&c);
-    x1=(-b+(sqrt(b*b-4*a*c)))/(2*a);
-    x2=(-b-(sqrt(b*b-4*a*c)))/(2*a);
-    if((pow(b,2)-(4*a*c)<0))
-       {
-        printf("no solutions");
-        }else if(a==0&&b!=0)
-        {
-            float x = (float)-c/b;
-            printf("the root is %f",x);
-        }else if(a&&b==0&&c!=0)
-        {
-            printf("no solutions");
-        }else if(a&&b==0&&c==0)
-        {
-            printf("infinitely many solutions");
-        }
-        else (x1!=x2? printf("the roots are %.1f and %.1f",x1,x2):
-              printf("the root is %.1f",x1) );
+    int a,b,c,min,max;
+    printf("Input 3 numbers:");
+    scanf("%d,%d,%d", &a, &b,&c);
+    (a<b&&a<c?  printf("The minimum value is %d\n",a):
+                       b<c? printf("The minimum value is %d\n",b):
+                       printf("The minimum value is %d\n",c));
+    (a>b&&a>c?  printf("The maximum value is %d\n",a):
+                       b>c? printf("The maximum value is %d\n",b):
+                       printf("The maximum value is %d\n",c));
     return 0;
 }
+
+
+
+
+
+
 
 
