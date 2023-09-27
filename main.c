1b0a0dbda8b0d19dada32584fcd2585220211e4a
panupongtony <146054375+panupongtony@users.noreply.github.com>
2023-09-27 11:43:28 +0700

    Add files via upload
    Roots of polynomials with 4 cases(27/9/23)

--- main.c
+++ main.c
@@ -11,9 +11,21 @@ int main()
     x2=(-b-(sqrt(b*b-4*a*c)))/(2*a);
     if((pow(b,2)-(4*a*c)<0))
        {
-        printf("NULL");
+        printf("no solutions");
+        }else if(a==0&&b!=0)
+        {
+            float x = (float)-c/b;
+            printf("the root is %f",x);
+        }else if(a&&b==0&&c!=0)
+        {
+            printf("no solutions");
+        }else if(a&&b==0&&c==0)
+        {
+            printf("infinitely many solutions");
         }
-        else printf("the roots are %.1f and %.1f",x1,x2);
+        else (x1!=x2? printf("the roots are %.1f and %.1f",x1,x2):
+              printf("the root is %.1f",x1) );
     return 0;
 }
+
 
