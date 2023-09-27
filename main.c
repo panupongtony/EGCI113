e2e169e3c1bd113b033df2c882fce1e415685080
panupongtony <146054375+panupongtony@users.noreply.github.com>
2023-09-27 11:47:39 +0700

    Polynomials(switch)(27/9/23)

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(){
+    float a,b,c;
+    float root1, root2, imaginary;
+    scanf("%f,%f,%f",&a,&b,&c);
+    float d = pow(b,2)-(4*a*c);
+    switch(d>0)
+    {
+    case 1:
+        root1 =(-b+(sqrt(d)))/(2*a);
+        root2=(-b-(sqrt(d)))/(2*a);
+        printf("the 2 distinct real roots are %.2f and %.2f",root1,root2);
+        break;
+    case 0:
+        switch(d<0)
+        {
 
-int main()
-{
-    int a,b,c,min,max;
-    printf("Input 3 numbers:");
-    scanf("%d,%d,%d", &a, &b,&c);
-    (a<b&&a<c?  printf("The minimum value is %d\n",a):
-                       b<c? printf("The minimum value is %d\n",b):
-                       printf("The minimum value is %d\n",c));
-    (a>b&&a>c?  printf("The maximum value is %d\n",a):
-                       b>c? printf("The maximum value is %d\n",b):
-                       printf("The maximum value is %d\n",c));
+            case 1:
+                root1 = root2 = -b/2*a;
+                imaginary = sqrt(-d)/2*a;
+                printf("the 2 imaginary real roots are %.2f + i%2.f and %.2f + i%2.f",root1,imaginary,root2,imaginary);
+            break;
+            case 0:
+                root1 = root2 = -b/2*a;
+                printf("the 2 same real roots are %.2f and %.2f",root1,root2);
+                break;
+        }
+
     return 0;
+    }
 }
-
-
-
-
-
-
-
 
